4159ba4b44ddc7af771376a68e354af6a06ea66c
Cameron Dawson <cdawson@mozilla.com>
2020-04-02 09:26:44 -0700

    work

--- treeherder/auth/backends.py
+++ treeherder/auth/backends.py
@@ -4,9 +4,9 @@ import time
 
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+from jose import jwt
 from rest_framework.exceptions import AuthenticationFailed
 
-from jose import jwt
 from treeherder.config.settings import (AUTH0_CLIENTID,
                                         AUTH0_DOMAIN)
 
